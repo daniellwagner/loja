1786e08a7e2f17f52239dd605f1491fb3ab8a849
danie <danie@DESKTOP-G06MAKC>
2018-08-04 16:50:09 -0300

    Aula 1 Terminada

--- loja/adiciona-produto.php
+++ loja/adiciona-produto.php
@@ -1,19 +1,7 @@
-<html>
-<head>
-<title>Minha loja</title>
-<meta charset="utf-8">
-<link href="css/bootstrap.css" rel="stylesheet" />
-<link href="css/loja.css" rel="stylesheet" />
-</head>
-<body>
-<div class="container">
-<div class="principal">
-<?php
- $nome = $_GET["nome"];
- $preco = $_GET["preco"];
-?>
-<p class="alert-success"> Produto <?= $nome; ?>, <?= $preco; ?> adicionado com sucesso! </p>
-</div>
-</div>
-</body>
-</html>
+<?php include 'cabecalho.php';?>
+    <?php
+     $nome = $_GET["nome"];
+     $preco = $_GET["preco"];
+    ?>
+    <p class="alert-success"> Produto <?= $nome; ?>, <?= $preco; ?> adicionado com sucesso! </p>
+<?php include 'rodape.php';?>
--- loja/produto-formulario.php
+++ loja/produto-formulario.php
@@ -1,20 +1,8 @@
-<html>
-<head>
-<title>Minha loja</title>
-<meta charset="utf-8">
-<link href="css/bootstrap.css" rel="stylesheet" />
-<link href="css/loja.css" rel="stylesheet" />
-</head>
-<body>
-<div class="container">
-<div class="principal">
+<?php include 'cabecalho.php';?>
 	<h1>Formulario de produto</h1>
 	<form action="adiciona-produto.php">
 		Nome: <input type="text" name="nome" /><br/>
 		Preco: <input type="number" name="preco" /> <br/>
 		<input type="submit" value="Cadastrar">
 	</form>
-</div>
-</div>
-</body>
-</html>
+<?php include 'rodape.php';?>
