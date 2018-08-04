77dc09a8774fba1e635e0ee564ca447c11ec4828
daniell <daniell_uepb@hotmail.com>
2018-08-04 14:56:17 -0300

    Primeiro commit. Aula 1 Incompleta

---
+++ loja/index.php
@@ -0,0 +1,15 @@
+<html>
+	<head>
+		<title>Minha Loja!</title>
+		<meta charset="utf-8">
+		<link href="css/bootstrap.css" rel="stylesheet">
+		<link href="css/loja.css" rel="stylesheet">
+	</head>
+	<body>
+		<div class="container">
+			<div class="principal">
+				<h1> Bem vindo! </h1>
+			</div>
+		</div>
+	</body>
+</html>
