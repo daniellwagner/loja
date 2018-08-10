4ffeec259adb6a93b9a1242c3ac6de228f2fabed
daniell.azevedo@gmail.com <daniell.azevedo@gmail.com>
2018-08-09 23:05:10 -0300

    Aula 3 Terminada

--- loja/adiciona-produto.php
+++ loja/adiciona-produto.php
@@ -1,19 +1,29 @@
 <?php include 'cabecalho.php';?>
     <?php
+    
+    function insereProduto($conexao, $nome, $preco) {
+        $query = "insert into produtos(nome, preco) values ('{$nome}', ${preco})";
+        return mysqli_query($conexao, $query);
+    }
+    
     $nome = $_GET["nome"];
     $preco = $_GET["preco"];
 
     $conexao = mysqli_connect('localhost', 'root', '', 'loja');
-    $query = "insert into produtos(nome, preco) values ('{$nome}', ${preco})";
-    if (mysqli_query($conexao, $query)) {
+    
+    if (insereProduto($conexao, $nome, $preco)) {
         ?>
-<p class="alert-success"> Produto <?= $nome; ?>, <?= $preco; ?> adicionado com sucesso! </p>
-<?php }else{ ?>
-<p class="alert-danger"> Produto <?= $nome; ?> nao foi adicionado! </p>
+		<p class="text-success"> Produto <?= $nome; ?>, <?= $preco; ?> adicionado com sucesso! </p>
+<?php }else{
+    
+    $msg = mysqli_error($conexao);
+    
+    ?>
+		<p class="text-danger"> Produto <?= $nome; ?> nao foi adicionado! <?= $msg?> </p>
 <?php
     }
-    mysqli_close($conexao);
 
     ?>
+    
     
 <?php include 'rodape.php';?>
--- loja/produto-formulario.php
+++ loja/produto-formulario.php
@@ -1,8 +1,28 @@
 <?php include 'cabecalho.php';?>
 	<h1>Formulario de produto</h1>
 	<form action="adiciona-produto.php">
-		Nome: <input type="text" name="nome" /><br/>
-		Preco: <input type="number" name="preco" /> <br/>
-		<input type="submit" value="Cadastrar">
+		<table class="table">
+			<tr>
+				<td>
+					Nome: 
+				</td>
+				<td>
+        			<input class="form-control" type="text" name="nome" /><br/>
+				</td>
+			</tr>
+			<tr>
+				<td>
+					Preco: 
+				</td>
+				<td>
+            		<input class="form-control" type="number" name="preco" /> <br/>
+				</td>
+			</tr>
+			<tr>
+				<td>
+            		<input class="btn btn-primary" type="submit" value="Cadastrar">
+				</td>
+			</tr>
+		</table>
 	</form>
 <?php include 'rodape.php';?>
