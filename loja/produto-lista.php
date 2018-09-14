afaade642cbbb2c40e808cc6bbf2904c17cf36a2
daniell.azevedo@gmail.com <daniell.azevedo@gmail.com>
2018-09-14 16:52:29 -0300

    Aula 10 Terminada

--- loja/produto-lista.php
+++ loja/produto-lista.php
@@ -20,7 +20,8 @@
     	<td><?= $produto['nome']?></td>
     	<td><?= $produto['preco']?></td>
     	<td><?= substr($produto['descricao'], 0, 40)?></td>
-    	<td><?=$produto['categoria_nome']?></td>        
+    	<td><?=$produto['categoria_nome']?></td>
+    	<td><a class="btn btn-primary" href="produto-altera-formulario.php?id=<?=$produto['id'] ?>">Alterar</a></td>        
     	<td>	
     		<form action="remove-produto.php?id=<?=$produto['id']?>" method="post">
     			<input type="hidden" name="id" value="<?= $produto['id']?>">
