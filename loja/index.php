f342058fb8c27972a38f214f37ed3e03e36e0cc8
daniell.azevedo@gmail.com <daniell.azevedo@gmail.com>
2018-09-21 19:37:24 -0300

    Parte 2 Aula 2 Terminada

--- loja/index.php
+++ loja/index.php
@@ -9,22 +9,26 @@
 
 	<h1> Bem vindo! </h1>
 	
-	<h2>Login</h2>
-	<form action="login.php" method="post">
-    	<table class="table">
-    		<tr>
-    			<td>Email</td>
-    			<td><input class="form-control" type="email" name="email"></td>
-    		<tr>
-    		
-    		<tr>
-    			<td>Senha</td>
-    			<td><input class="form-control" type="password" name="senha"> </td>
-    		</tr>
-    		
-    		<tr>
-    			<td><button class="btn btn-primary">Login</button></td>
-    		</tr>
-    	</table>
-	</form>
+	<?php if(isset($_COOKIE["usuario_logado"])) {?>
+		<p class="text-success"> Voce esta logado como <?=$_COOKIE["usuario_logado"]?>.</p> 
+	<?php } else { ?>
+    	<h2>Login</h2>
+    	<form action="login.php" method="post">
+        	<table class="table">
+        		<tr>
+        			<td>Email</td>
+        			<td><input class="form-control" type="email" name="email"></td>
+        		<tr>
+        		
+        		<tr>
+        			<td>Senha</td>
+        			<td><input class="form-control" type="password" name="senha"> </td>
+        		</tr>
+        		
+        		<tr>
+        			<td><button class="btn btn-primary">Login</button></td>
+        		</tr>
+        	</table>
+    	</form>
+    <?php }?>
 <?php include 'rodape.php';?>
--- loja/login.php
+++ loja/login.php
@@ -2,11 +2,13 @@
 include("banco-usuario.php");
 
 $usuario = buscaUsuario($conexao, $_POST["email"], $_POST["senha"]);
+
 var_dump($usuario);
 
 if($usuario == null){
     header("Location: index.php?login=0");
 } else {
+    setcookie("usuario_logado", $usuario["email"], time() + 60);
     header("Location: index.php?login=1");
 }
 
